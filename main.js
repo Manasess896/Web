b0f0f321aaeef09eec0061d4b815672f8bf4aa03
Website 18 <147276750+Manasess896@users.noreply.github.com>
2023-12-10 23:13:29 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+        document.getElementById("mySidenav").style.display = "block";
+        document.getElementById("main").style.marginLeft = "100%px";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.display = "none";
+        document.getElementById("main").style.marginLeft = "0";
+    }
