26b9fa377aa57dda0f7ad8a82dbb0f21095bcf1d
Website 18 <147276750+Manasess896@users.noreply.github.com>
2024-01-03 11:24:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -23,6 +23,34 @@ var slideshows = document.querySelectorAll('[data-component="slideshow"]');
 
     }, time);
   }
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+
+
 function openNav() {
         document.getElementById("mySidenav").style.display = "block";
         document.getElementById("main").style.marginLeft = "100%px";
@@ -42,4 +70,7 @@ showSlides(slideIndex);
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
+}
+
+
 
