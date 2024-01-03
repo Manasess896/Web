c225eda2135572d0ed123fd2e0f16b9bb4431d90
Website 18 <147276750+Manasess896@users.noreply.github.com>
2024-01-03 11:11:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-
-function openNav() {
-        document.getElementById("mySidenav").style.display = "block";
-        document.getElementById("main").style.marginLeft = "100%px";
-    }
-
-    function closeNav() {
-        document.getElementById("mySidenav").style.display = "none";
-        document.getElementById("main").style.marginLeft = "0";
-    }
-
-
-
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-
-
 var slideshows = document.querySelectorAll('[data-component="slideshow"]');
   
   // Apply to all slideshows that you define with the markup wrote
@@ -47,24 +23,23 @@ var slideshows = document.querySelectorAll('[data-component="slideshow"]');
 
     }, time);
   }
+function openNav() {
+        document.getElementById("mySidenav").style.display = "block";
+        document.getElementById("main").style.marginLeft = "100%px";
+    }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    function closeNav() {
+        document.getElementById("mySidenav").style.display = "none";
+        document.getElementById("main").style.marginLeft = "0";
+    }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+
